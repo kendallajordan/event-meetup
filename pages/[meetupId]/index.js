985628763036ab60dc42a418285006bc052a1823
Kendall Jordan <kendallajordan@gmail.com>
2023-05-04 21:46:34 -0700

    Added Meetup Detail component to render meetup details through props. MeetupDetails Page renders Meetup Detail component. Added MeetupDetail CSS Module.

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,1 +1,15 @@
 // our-domain.com/meetupId
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
+      title="First Meeting"
+      address="Some Street 5, Some City"
+      description="This is a first meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
